57ff6fca744598925f5a9eaccc2d0f4ff6c16e52
kimnanhee <kimnanhee0225@gmail.com>
2020-12-26 23:22:30 +0900

    [Add] symmetry image

---
+++ symmetry.py
@@ -0,0 +1,12 @@
+import cv2
+
+image = cv2.imread("IMAGE/nanhee.jpg", cv2.IMREAD_COLOR)
+image_flip_1 = cv2.flip(image, 0) # 상하
+image_flip_2 = cv2.flip(image, 1) # 좌우
+
+cv2.imshow("image", image)
+cv2.imshow("image_flip_1", image_flip_1)
+cv2.imshow("image_flip_2", image_flip_2)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
