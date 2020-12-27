238173e5a98bb09df1c9656eafb416437e7c9802
kimnanhee <kimnanhee0225@gmail.com>
2020-12-27 22:58:25 +0900

    [Add] grayscale.py

---
+++ grayscale.py
@@ -0,0 +1,10 @@
+import cv2
+
+image = cv2.imread("IMAGE/nanhee.jpg", cv2.IMREAD_COLOR)
+image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("image", image)
+cv2.imshow("image_gray", image_gray)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
