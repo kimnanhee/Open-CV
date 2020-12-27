74e0edee25546dd244ba72c6c3e2dc849d4b2b55
kimnanhee <kimnanhee0225@gmail.com>
2020-12-27 17:23:46 +0900

    [Add] slice.py

---
+++ slice.py
@@ -0,0 +1,12 @@
+import cv2
+
+image = cv2.imread("IMAGE/nanhee.jpg", cv2.IMREAD_COLOR)
+
+image_slice = image.copy()
+image_slice = image[150:550, 200:550] # 높이, 너비
+
+cv2.imshow("image", image)
+cv2.imshow("image_slice", image_slice)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
