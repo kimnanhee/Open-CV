4a76bde306b2704d5b867c3788b5c0763de3facf
kimnanhee <kimnanhee0225@gmail.com>
2021-01-02 01:20:06 +0900

    [Modify] HSV.py - find orange color

--- HSV.py
+++ HSV.py
@@ -7,12 +7,16 @@ image = cv2.resize(image, dsize=(0, 0), fx=0.6, fy=0.6) # 0.6배 비율 조절
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) # HSV채널로 변경
 h, s, v = cv2.split(hsv) # 각각의 속성으로 분할
 
+h = cv2.inRange(h, 8, 20)
+orange = cv2.bitwise_and(hsv, hsv, mask = h)
+orange = cv2.cvtColor(orange, cv2.COLOR_HSV2BGR)
 
 cv2.imshow("image", image)
 # cv2.imshow("hsv", hsv)
 # cv2.imshow("h", h)
 # cv2.imshow("s", s)
 # cv2.imshow("v", v)
+cv2.imshow("orange", orange)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
